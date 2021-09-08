72c4c0d78edff66476d033cfb47fc8fa68e4f93b
aloicerains <aloiceokoth98@gmail.com>
2021-09-08 21:47:11 +0300

    new changes

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,16 +2,11 @@
 def fizzbuzz():
     """Function prints the formated numbers 1 to 100."""
     for i in range(1, 101):
-        if i % 15 == 0 and i < 100:
-            print('FizzBuzz', end=', ')
-            continue
-        elif i % 5 == 0 and i < 100:
-            print('Buzz', end=', ')
-            continue
-        elif i % 3 == 0 and i < 100:
-            print('Fizz', end=', ')
-            continue
-        elif i < 100:
-            print('{}'.format(i), end=', ')
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif i % 3 == 0:
+            print("Fizz ", end="")
+        elif i % 5 == 0:
+            print("Buzz ", end="")
         else:
-            print('Buzz', end='')
+            print("{} ".format(i), end="")
