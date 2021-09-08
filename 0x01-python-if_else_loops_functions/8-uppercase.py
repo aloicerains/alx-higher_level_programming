3f99cab5727b3797599c69e2a3c4afa0ec99347c
aloicerains <aloiceokoth98@gmail.com>
2021-09-08 15:13:43 +0300

    reduced more print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,12 +5,6 @@ def uppercase(str):
     for i in range(limit):
         if (str[i] >= 'a' and str[i] <= 'z'):
             conversion = ord(str[i]) - 32
-            if i == limit - 1:
-                print('{:c}'.format(conversion))
-                break
             print('{:c}'.format(conversion), end='')
         else:
-            if i == limit - 1:
-                print('{}'.format(str[i]))
-                break
             print('{}'.format(str[i]), end='')
