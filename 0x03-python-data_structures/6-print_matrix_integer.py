7b773128b79dd2096ced76bc43c03ff4ea98b23e
aloicerains <aloiceokoth98@gmail.com>
2021-09-13 11:47:23 +0300

    updated

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,9 @@ def print_matrix_integer(matrix=[[]]):
     """Function prints a matrix of integers."""
     for row in matrix:
         for i in row:
+            if i == row[len(row) - 1]:
+                print("{}".format(i))
+                continue
             print('{}'.format(i), end=" ")
+    if len(row) == 0:
         print()
