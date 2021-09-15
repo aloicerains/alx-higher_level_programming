eb71aa1c69007b62722e47384df583b5d0a8c64f
aloicerains <aloiceokoth98@gmail.com>
2021-09-15 16:08:15 +0300

    removed conditions

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     """Function adds unique integers in a list only once."""
-    if set_1 and set_2 is not None:
-        new_list = set(set_1).intersection(set_2)
-        return new_list
+    new_list = set(set_1).intersection(set_2)
+    return new_list
