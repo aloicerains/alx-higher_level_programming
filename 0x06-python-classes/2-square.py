9ff705b47654663b1a1f225bf55c769069c70263
aloicerains <aloiceokoth98@gmail.com>
2021-09-28 22:37:58 +0300

    updated

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,24 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+"""Square module - assigns square size and checks for errors."""
+
+
 class Square:
     """Square defines the object square using private attribute __size.
-    Args:
-        __size: Variable argument without specified type.
+
 
     Attributes:
         __size: Instance variable of class square.
 
+
         """
-    def __init__(self, __size=0):
-        try:
-            if not isinstance(__size, int):
-                raise TypeError
-            if __size < 0:
-                raise ValueError
-        except TypeError:
-            print("size must be an integer")
-            return
-        except ValueError:
-            print("size must be >= 0")
-            return
-        else:
-            self.__size = __size
+
+    def __init__(self,size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,8 +1,15 @@
 #### 0x06-python-classes Directory    
 The directory contains files and programs on python classes and objects.  
 The following tasks are present in the directory:   
+
 - [] **0-square.py**   
 An empty class `square` defining the a square.   
+
 - [] **1-square.py**   
 A class square that defines a square by attribute size.   
 Size is a private instance attribute   
+
+- [] **2-square.py**  
+Class square defines which defined by instance of size.  
+Size is always an integer else a `TypeError` is thrown or a `ValueError` result when size is less than 0   
+
