0f7c9f6720726d99325521316a2364e653de3132
aloicerains <aloiceokoth98@gmail.com>
2021-09-28 18:34:53 +0300

    imported square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
-class Square:
-    """class Square with size."""
+Square = __import__('0-square').Square
+
+
+class Square():
+    """Square defines the object square using private attribute __size.
+    Args:
+        __size: Variable argument without specified type.
+
+    Attributes:
+        __size: Instance variable of class square.
+        .
+    """
     def __init__(self, __size):
-        if __size is not None:
-            self.__size = __size
+        self.__size = __size
