daf7d8c7dbc5f6302cf92996eb5a6e3416255e23
aloicerains <aloiceokoth98@gmail.com>
2021-10-04 16:21:23 +0300

    task one

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+
+
+class Rectangle:
+    """Rectangle class gives basic description of a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """Obtains and sets the width of rectangle.
+
+        Raises:
+            TypeError: When the widht is not integer
+            ValueError: When width < 0
+
+        """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets width of the rectangle"""
+
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """Obtains the height of the rectangle.
+
+        Raises:
+            TypeError: when height is not integer.
+            ValueError: When height < 0
+
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the value of rectangle height"""
+
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -1,6 +1,13 @@
 #### 0x08-python-more_classes Directory   
 The following tasks are handled in the directory:    
-- [ ] **0-rectangle.py**   
+- [x] **0-rectangle.py**   
 Creates an empty class `Rectangle` that defines a rectangle:   
 	* No module is imported   
+- [ ] **1-rectangle.py**   
+Class Rectangle has been given setter and getter methods for both height and width.    
+* If height is not an integer:   
+	`raise TypeError`   
+* If height is less than 0:   
+	`raise ValueError`  
+* An `__init__` method instantiates widhth and height to zero.  
 
