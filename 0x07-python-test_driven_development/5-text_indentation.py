58a91fcd723d91e978505193d0f313988990976c
aloicerains <aloiceokoth98@gmail.com>
2021-10-06 13:33:23 +0300

    updated task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """text_indentation module"""
+
 
 def text_indentation(text):
     """text_indentation prints two new lines after sysmbols.
@@ -13,27 +14,7 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError('text must be a string')
-    flag = 0
-    bsp = ''
-    for i in text:
-        if i == '.':
-            flag = 1
-            print(i, end='')        
-            continue
-        elif i == '?':
-            flag = 1
-            print(i, end='')
-            continue
-        elif i == ':':
-            flag = 1
-            print(i, end='')
-            continue
-        else:
-            if flag == 1:
-                print('')
-                print('')
-            if flag == 1 and i == " ":
-                flag = 0
-                continue
-            print(i, end='')
-            flag = 0
+    for delimiter in ".?:":
+        text = (delimiter + "\n\n").join([line.strip(" ")
+                                         for line in text.split(delimiter)])
+    print(text, end='')
