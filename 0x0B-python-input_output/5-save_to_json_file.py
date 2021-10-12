362824ec7f3f3b234c85dd77e0510b908f4d7b2c
aloicerains <aloiceokoth98@gmail.com>
2021-10-12 18:37:09 +0300

    created file first

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,6 +12,6 @@ def save_to_json_file(my_obj, filename):
         filename(obj): file name
 
     """
-    text = json.dumps(my_obj)
     with open(filename, 'w') as f:
+        text = json.dumps(my_obj)
         f.write(text)
