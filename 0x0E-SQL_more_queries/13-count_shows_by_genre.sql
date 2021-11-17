691aab9b7c40e98416890bf28ed19b6971304528
aloicerains <aloiceokoth98@gmail.com>
2021-11-18 00:34:38 +0300

    task 13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- script list all genres and displays shows linked to each other
+SELECT tv_genres.name FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter" 
+ORDER BY tv_genres.name DESC;
