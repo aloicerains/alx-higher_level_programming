00faf6ae03940cbc8ac5836ac7cc84afd1e3f226
aloicerains <aloiceokoth98@gmail.com>
2021-11-17 23:40:53 +0300

    made changes

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,3 @@
 -- user have all previleges on the server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT USAGE ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
