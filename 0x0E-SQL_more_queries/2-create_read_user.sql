ac254e7ead2491aa7b91c16071844e0ffc93db35
aloicerains <aloiceokoth98@gmail.com>
2021-12-10 21:10:23 +0300

    updated

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 -- creates a database and user
 -- user has select privileges
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_02'@'localhost' IDENTIFIED BY 'user_od_2_pwd';
-GRANT SELECT ON hbtn_0d_2 .* TO 'user_od_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2 .* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
