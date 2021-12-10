2f301712cc4d6675c9f5293f4376d92deda5057f
aloicerains <aloiceokoth98@gmail.com>
2021-12-10 20:45:43 +0300

    added all

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Create MySQL server user
 -- user have all previleges on the server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT USAGE ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
