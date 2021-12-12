fbd9060a74337fd1874a634e4bb512264d24e470
aloicerains <aloiceokoth98@gmail.com>
2021-12-12 13:58:43 +0300

    task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
--- script list all genres and displays shows linked to each other
-SELECT tv_genres.name FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = "Dexter" 
-ORDER BY tv_genres.name DESC;
+-- displsys he number of shows liked to ech genres from the hbtn_0d_tvshows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
