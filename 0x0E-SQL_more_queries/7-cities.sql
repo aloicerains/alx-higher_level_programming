0f60cf7dabd4c1b51240caf7c0969bd7a97f6c3d
aloicerains <aloiceokoth98@gmail.com>
2021-12-12 12:48:29 +0300

    updated

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 -- creating database and table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-	id INT AUTO_INCREMENT UNIQUE NOT NULL,
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL UNIQUE,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (state_id) REFERENCES hbtn_0d.states(id));
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
 
