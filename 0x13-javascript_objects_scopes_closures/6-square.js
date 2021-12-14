b90bf3d4b5aa10e9088b72cf7b39a8607ba68d80
aloicerains <aloiceokoth98@gmail.com>
2021-12-14 17:35:24 +0300

    updated

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const Square0 = require('./5-square');
+class Square extends Square0 {
   charPrint (c) {
     if (c === undefined) {
       super.print();
