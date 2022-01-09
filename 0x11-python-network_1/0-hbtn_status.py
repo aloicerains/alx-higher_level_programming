b371e06646372b01a9fba016ef56c6deb20fa286
aloicerains <aloiceokoth98@gmail.com>
2022-01-09 14:22:35 +0300

    updated spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,11 @@
 
 
 if __name__ == "__main__":
-    import urllib.request
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+    from urllib import request
+
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
         page = response.read()
         print("Body response:")
-        print("\t- type:", page.__class__)
-        print("\t- content:", page)
-        print("\t- utf8 content:", page.decode("utf-8"))
+        print("    - type:", page.__class__)
+        print("    - content:", page)
+        print("    - utf8 content:", page.decode("utf-8"))
